0a4663332c1e02f9c19e82135c3be8a0ef0e63f3
bipangshu <bumba8812@gmail.com>
2023-07-08 14:12:12 +0530

    first commit

---
+++ Search Page/index.js
@@ -0,0 +1,14 @@
+let prevBtn = document.querySelector(".left-arrow");
+let nextBtn = document.querySelector(".right-arrow");
+
+let cardContainer = document.querySelector(".cards-container");
+
+let containerWidth = cardContainer.getBoundingClientRect().width;
+
+prevBtn.addEventListener("click", () => {
+    cardContainer.scrollLeft -= (containerWidth*0.25);
+})
+
+nextBtn.addEventListener("click", () => {
+    cardContainer.scrollLeft += (containerWidth*0.25);
+})
